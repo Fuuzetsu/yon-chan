7aec9c3e775b592b9705dc52f8853bcc7a604559
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-05-03 18:59:04 +0100

    More watchr-friendly output

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -e
 ECUKES=$(find elpa/ecukes-*/ecukes | tail -1)
+
 carton exec "$ECUKES" "$@"
+if [[ "$TRAVIS" == "true" ]]; then
+    exit $?
+fi
+
+if [ $? -eq 0 ]; then
+    echo Ecukes tests passing.
+fi
