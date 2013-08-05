5ad59174c1e1acaa4aca7d65172bd488a98057e6
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-08-05 18:25:21 +0100

    Add own path to load dir

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -3,9 +3,9 @@ EMACS=${ECUKES_EMACS:-emacs}
 PRINT_ANYWAY="true"
 
 if [[ "$TRAVIS" == "true" || "$PRINT_ANYWAY" == "true" ]]; then
-    $EMACS -batch -l ert -l elpa/dash*/dash.el -l elpa/mocker*/mocker.el -l yon-chan.el -l yon-chan-tests.el -f ert-run-tests-batch-and-exit
+    $EMACS -batch -l ert -l elpa/dash*/dash.el -l elpa/mocker*/mocker.el -L . -l yon-chan-tests.el -f ert-run-tests-batch-and-exit
 else
-    OUTPUT=$("$EMACS" -batch -l ert -l elpa/dash*/dash.el -l elpa/mocker*/mocker.el -l yon-chan.el -l yon-chan-tests.el -f ert-run-tests-batch-and-exit 2>&1)
+    OUTPUT=$("$EMACS" -batch -l ert -l elpa/dash*/dash.el -l elpa/mocker*/mocker.el -L . -l yon-chan-tests.el -f ert-run-tests-batch-and-exit 2>&1)
     STATUS=$?
     if [ $STATUS -ne 0 ]; then
         echo "${OUTPUT}"
